5a41ea6d6cde02b86b885a8f09175e0c419680a9
Gianluca-Sasanelli <131895795+Gianluca-Sasanelli@users.noreply.github.com>
2024-08-24 11:38:38 +0200

    Final update of sample

--- sample.py
+++ sample.py
@@ -1,23 +1,26 @@
 #Sample
-import torch, os,tiktoken
+import torch, os,tiktoken, pickle
 from model import GPT
 #----------------------------------------------------------------
-init_from = "scatch"
+init_from = "resume"
 out_dir = "outputs"
-#sampling durining training
-start = "<|endoftext|>"
-num_samples = 10
-max_new_tokens = 500 # number of tokens generated in each sample
-temperature = 1.0
+#sampling method
+start = "\n"
+num_samples = 3
+max_new_tokens = 250 # number of tokens generated in each sample
+temperature = 0.5
 top_k = 200 #retain onlt the first topo k topkens, the othe will have probabilities of 0
 #system
 seed = 42
 device = "cuda"
 dtype = "bfloat16"
 compile = False
+#utils
+data_dir = r"C:\Users\Gianl\Desktop\gpt-repository\data"
+dataset = "dailymails"
+meta = data_dir + os.sep + dataset + "-meta.pkl" if os.path.exists(data_dir + os.sep + dataset + "-meta.pkl") else None
 #----------------------------------------------------------------
 torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
 device_type = "cuda"
 ptdtype = torch.bfloat16
 ctx = torch.amp.autocast(device_type = device_type, dtype = ptdtype)
@@ -37,15 +40,34 @@ if init_from == 'resume':
     model.load_state_dict(state_dict)
 model.eval()
 model.to(device)
+if meta:
+    with open(meta, "rb") as f:
+        meta = pickle.load(f)
+        decode = meta["itos"]
+        encode = meta["stoi"]
+        vocab_size = meta["vocab_size"]
+        start_ids = encode[start]
+else:
+    import tiktoken
+    tokenizer = tiktoken.get_encoding("gpt2")
+    start_ids = tokenizer.encode(start, allowed_special={start})
+    
+x = torch.tensor(start_ids, dtype = torch.int64, device = device).repeat(num_samples,1)
 
-tokenizer = tiktoken.get_encoding("gpt2")
-start_ids = tokenizer.encode(start, allowed_special={start})
-print(start_ids)
-x = torch.tensor(start_ids, dtype = torch.int64, device = device) [None, ...]
 with torch.no_grad():
     with ctx:
-        for k in range(num_samples):
             y = model.generate(x, max_new_tokens, temperature = temperature, top_k = top_k)
-            print(tokenizer.decode(y[0].tolist()))
-            print("------------------------------")
+            if not meta:
+                for i in range(y.shape[0]):
+                    
+                    print(tokenizer.decode(y[i].tolist()))
+                    print("------------------------------")
+            else:
+                text = []
+                for i in range(y.shape[0]):
+                    for idx in y[i].tolist():
+                        text.extend(decode[idx])
+                    print("".join(text))
+                    text = []
+                    print("------------------------------")
 
